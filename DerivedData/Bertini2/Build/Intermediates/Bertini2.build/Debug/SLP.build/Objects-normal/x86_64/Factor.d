a7123f534f8cb169d3bc479e262673b4c179513e
Jeb Collins <jbcolli2@gmail.com>
2014-10-16 11:12:15 -0500

    Nothing

--- DerivedData/Bertini2/Build/Intermediates/Bertini2.build/Debug/SLP.build/Objects-normal/x86_64/Factor.d
+++ DerivedData/Bertini2/Build/Intermediates/Bertini2.build/Debug/SLP.build/Objects-normal/x86_64/Factor.d
@@ -1,6 +1,6 @@
 dependencies: \
-  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2_MainRepo/SLP/Factor.cpp \
-  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2_MainRepo/SLP/Factor.h \
+  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2MyFork/SLP/Factor.cpp \
+  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2MyFork/SLP/Factor.h \
   /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/boost_1_56_0/boost/numeric/ublas/matrix.hpp \
   /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/boost_1_56_0/boost/numeric/ublas/vector.hpp \
   /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/boost_1_56_0/boost/numeric/ublas/storage.hpp \
@@ -447,11 +447,11 @@ dependencies: \
   /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/boost_1_56_0/boost/numeric/ublas/matrix_expression.hpp \
   /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/boost_1_56_0/boost/numeric/ublas/detail/matrix_assign.hpp \
   /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/boost_1_56_0/boost/numeric/ublas/io.hpp \
-  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2_MainRepo/SLP/Operand.h \
-  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2_MainRepo/SLP/Symbol.h \
-  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2_MainRepo/SLP/DoubleSymb.h \
-  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2_MainRepo/SLP/IntSymb.h \
-  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2_MainRepo/SLP/AdditionOp.h \
-  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2_MainRepo/SLP/MultOp.h \
-  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2_MainRepo/SLP/ExpOp.h \
-  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2_MainRepo/SLP/LeafOperand.h
+  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2MyFork/SLP/Operand.h \
+  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2MyFork/SLP/Symbol.h \
+  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2MyFork/SLP/DoubleSymb.h \
+  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2MyFork/SLP/IntSymb.h \
+  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2MyFork/SLP/AdditionOp.h \
+  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2MyFork/SLP/MultOp.h \
+  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2MyFork/SLP/ExpOp.h \
+  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2MyFork/SLP/LeafOperand.h
