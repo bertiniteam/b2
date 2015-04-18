c6be69c67e411ca9dab1ce39120dec78dadc18e7
Jeb Collins <jbcolli2@gmail.com>
2015-04-18 17:20:42 -0500

    MPFR1.0

---
+++ QiPlay/MPFR/MPFR_ParserTest.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MPFR
+//
+//  Created by Collins, James B. on 4/11/15.
+//  Copyright (c) 2015 West Texas A&M University. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
