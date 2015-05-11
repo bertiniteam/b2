0f15fe528fb5dfa7ba55c6ab7f37128808da04c7
Jeb Collins <jbcolli2@gmail.com>
2015-05-11 15:41:33 -0500

    Combine mult and divide in MultOperator

--- b2Test/Function/mult_operator.h
+++ b2Test/Function/mult_operator.h
@@ -25,33 +25,80 @@ public:
     std::string PrintNode() override {return "";}
 
 
+    //Special Behaviour: by default all factors are in numerator
+    virtual void AddChild(std::shared_ptr<Node> child) override
+    {
+        NaryOperator::AddChild(std::move(child));
+        children_mult_.push_back(true);
+    }
+    
+    
+    
+    //Special Behaviour: Pass bool to set sign of term: true = mult, false = divide
+    void AddChild(std::shared_ptr<Node> child, bool mult)
+    {
+        NaryOperator::AddChild(std::move(child));
+        children_mult_.push_back(mult);
+    }
 
 
 
 
 protected:
-    // Specific implementation of FreshEval for multiply.
+    // Specific implementation of FreshEval for mult and divide.
+    //  If child_mult_ = true, then multiply, else divide
     dbl FreshEval(dbl) override
     {
         dbl retval{1};
-        for(auto& vv : children_)
+        for(int ii = 0; ii < children_.size(); ++ii)
         {
-            retval *= vv->Eval<dbl>();
+            if(children_mult_[ii])
+            {
+                retval *= children_[ii]->Eval<dbl>();
+            }
+            else
+            {
+                retval /= children_[ii]->Eval<dbl>();
+            }
         }
         
         return retval;
     }
     
+    
+    
+    
     mpfr FreshEval(mpfr) override
     {
         mpfr retval{1};
-        for(auto& vv : children_)
+        for(int ii = 0; ii < children_.size(); ++ii)
         {
-            retval *= vv->Eval<mpfr>();
+            if(children_mult_[ii])
+            {
+                retval *= children_[ii]->Eval<mpfr>();
+            }
+            else
+            {
+                retval /= children_[ii]->Eval<mpfr>();
+            }
         }
         
         return retval;
     }
+    
+    
+    
+    
+    
+    
+private:
+    // Stores the mult/div of a factor.  There is a one-one
+    // correspondence between elements of children_sign_ and children_.  This
+    // is enforced by the AddChild method, redefined in MultOperator.
+    
+    // TODO(JBC): If we add method to delete child, must also delete children_mult_ entry.
+    std::vector<bool> children_mult_;
+
 };
 
 
