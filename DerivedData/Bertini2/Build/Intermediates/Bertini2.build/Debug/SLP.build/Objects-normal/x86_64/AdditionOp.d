2fef60a2e5851fe3c5848ef5a1d912ba694b865f
BTeam -- Dan Bates <bates@math.colostate.edu>
2015-05-22 15:07:01 -0600

    removed old stale files from the develop branch, including those which started with Test*, and the Xcode projects, etc.

--- DerivedData/Bertini2/Build/Intermediates/Bertini2.build/Debug/SLP.build/Objects-normal/x86_64/AdditionOp.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2_MainRepo/SLP/AdditionOp.cpp \
-  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2_MainRepo/SLP/LeafOperand.h \
-  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2_MainRepo/SLP/Operand.h \
-  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2_MainRepo/SLP/Symbol.h \
-  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2_MainRepo/SLP/DoubleSymb.h \
-  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2_MainRepo/SLP/IntSymb.h \
-  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2_MainRepo/SLP/AdditionOp.h
--- DerivedData/Bertini2/Build/Intermediates/Bertini2.build/Debug/SLP.build/Objects-normal/x86_64/DoubleSymb.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2_MainRepo/SLP/DoubleSymb.cpp \
-  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2_MainRepo/SLP/DoubleSymb.h \
-  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2_MainRepo/SLP/Symbol.h \
-  /Users/jcollins/Documents/West\ Texas/Research/Bertini/Code/b2_MainRepo/SLP/IntSymb.h
