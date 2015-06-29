a853040d012b02ed429be128df0ad2fa9820f8ca
daniel brake <danielthebrake@gmail.com>
2015-06-29 12:24:18 -0400

    initial addition of Diff code for system.

--- core/include/system.hpp
+++ core/include/system.hpp
@@ -38,58 +38,60 @@
 
 
 namespace bertini {
-	
+
 	template<typename T>
 	using Vec = Eigen::Matrix<T, Eigen::Dynamic, 1>;
-	
+	template<typename T>
+	using Mat = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>;
 	/**
 	 The fundamental polynomial system class for Bertini2.
 	 */
 	class System{
-		
-		
+
+
 		// a few local using statements to reduce typing etc.
 		using Fn = std::shared_ptr<Function>;
 		using Var = std::shared_ptr<Variable>;
 		using Nd = std::shared_ptr<Node>;
-		
+		using Jac = std::shared_ptr<Jacobian>;
+
 	public:
-		
+
 		void precision(unsigned new_precision)
 		{
 			for (auto iter : functions_) {
 //				iter->precision(new_precision);
 			}
-			
+
 			for (auto iter : subfunctions_) {
 				//				iter->precision(new_precision);
 			}
-			
+
 			for (auto iter : explicit_parameters_) {
 				//				iter->precision(new_precision);
 			}
-			
+
 			for (auto iter : variables_) {
 				//				iter->precision(new_precision);
 			}
-			
+
 			for (auto iter :implicit_parameters_) {
 				//				iter->precision(new_precision);
 			}
-			
+
 			for (auto iter : constant_subfunctions_) {
 				//				iter->precision(new_precision);
 				//				iter->eval<>()
 			}
-			
+
 //			path_variable_->precision(new_precision);
-			
+
 			precision_ = new_precision;
 		}
-		
-		
-		
-		
+
+
+
+
 		/**
 		 Get the number of functions in this system
 		 */
@@ -97,11 +99,11 @@ namespace bertini {
 		{
 			return functions_.size();
 		}
-		
-		
-		
-		
-		
+
+
+
+
+
 		/**
 		 Get the number of variables in this system
 		 */
@@ -109,7 +111,7 @@ namespace bertini {
 		{
 			return variables_.size();
 		}
-		
+
 		/**
 		 Get the number of constants in this system
 		 */
@@ -117,7 +119,7 @@ namespace bertini {
 		{
 			return constant_subfunctions_.size();
 		}
-		
+
 		/**
 		 Get the number of explicit parameters in this system
 		 */
@@ -125,8 +127,8 @@ namespace bertini {
 		{
 			return explicit_parameters_.size();
 		}
-		
-		
+
+
 		/**
 		 Get the number of implicit parameters in this system
 		 */
@@ -134,46 +136,76 @@ namespace bertini {
 		{
 			return implicit_parameters_.size();
 		}
-		
-		
+
+
 		/**
 		 Evaluate the system.
 		 */
 		template<typename T>
 		Vec<T> Eval(const Vec<T> & variable_values, const T & path_variable_value)
 		{
-			
+
 			assert(variable_values.size()==NumVariables());
-			
+
 			// this function call traverses the entire tree, resetting everything.
 			//
-			// it has the unfortunate side effect of resetting constant functions, too.
+			// TODO: it has the unfortunate side effect of resetting constant functions, too.
 			//
 			// we need to work to correct this.
 			for (auto iter : functions_) {
 				iter->Reset();
 			}
-			
+
 			SetVariables(variable_values);
 			SetPathVariable(path_variable_value);
-			
-			
+
+
 			Vec<T> value(NumFunctions()); // create vector with correct number of entries.
-			
+
 			{ // for scoping of the counter.
 				auto counter = 0;
 				for (auto iter=functions_.begin(); iter!=functions_.end(); iter++, counter++) {
 					value(counter) = (*iter)->Eval<T>();
 				}
 			}
-			
+
 			return value;
 		}
-		
-		
-		
-		
-		
+
+
+
+		template<typename T>
+		Mat<T> Jacobian(const Vec<T> & variable_values, const T & path_variable_value)
+		{
+			assert(variable_values.size()==NumVariables());
+
+			if (!is_differentiated_)
+			{
+				jacobian_.resize(NumFunctions());
+				for (int ii = 0; ii < NumFunctions(); ++ii)
+				{
+					jacobian_[ii] = std::make_shared<bertini::Jacobian>(functions_[ii]->Differentiate());
+				}
+				is_differentiated_ = true;
+			}
+
+			SetVariables(variable_values);
+			SetPathVariable(path_variable_value);
+
+			Mat<T> J(NumFunctions(), NumVariables());
+			for (int ii = 0; ii < NumFunctions(); ++ii)
+			{
+				for (int jj = 0; jj < NumVariables(); ++jj)
+				{
+					J(ii,jj) = jacobian_[ii]->EvalJ<T>(variables_[jj]);
+				}
+			}
+
+
+		}
+
+
+
 		/**
 		 Set the values of the variables to be equal to the input values
 		 */
@@ -181,18 +213,18 @@ namespace bertini {
 		void SetVariables(const Vec<T> & new_values)
 		{
 			assert(new_values.size()== variables_.size());
-			
+
 			{ // for scoping of the counter.
 				auto counter = 0;
 				for (auto iter=variables_.begin(); iter!=variables_.end(); iter++, counter++) {
 					(*iter)->set_current_value(new_values(counter));
 				}
 			}
-			
+
 		}
-		
-		
-		
+
+
+
 		/**
 		 Set the current value of the path variable.
 		 */
@@ -201,10 +233,10 @@ namespace bertini {
 		{
 			path_variable_->set_current_value(new_value);
 		}
-		
-		
-		
-		
+
+
+
+
 		/**
 		 For a system with implicitly defined parameters, set their values.  The values are determined externally to the system, and are tracked along with the variables.
 		 */
@@ -212,7 +244,7 @@ namespace bertini {
 		void SetImplicitParameters(Vec<T> new_values)
 		{
 			assert(new_values.size()== implicit_parameters_.size());
-			
+
 			{
 				auto counter = 0;
 				for (auto iter=implicit_parameters_.begin(); iter!=implicit_parameters_.end(); iter++, counter++) {
@@ -220,14 +252,14 @@ namespace bertini {
 				}
 			}
 		}
-		
-		
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
+
+
 		/**
 		 Add a variable group to the system.  The system will be homogenized with respect to this variable group, though this is not done at the time of this call.
 		 */
@@ -236,8 +268,8 @@ namespace bertini {
 			variable_groups_.push_back(v);
 			variables_.insert( variables_.end(), v.begin(), v.end() );
 		}
-		
-		
+
+
 		/**
 		 Add a homogeneous (projective) variable group to the system.  The system must be homogeneous with respect to this group, though this is not verified at the time of this call.
 		 */
@@ -246,8 +278,8 @@ namespace bertini {
 			hom_variable_groups_.push_back(v);
 			variables_.insert( variables_.end(), v.begin(), v.end() );
 		}
-		
-		
+
+
 		/**
 		 Add variables to the system which are in neither a regular variable group, nor in a homogeneous group.  This is likely used for user-defined systems, Classic userhomotopy: 1;.
 		 */
@@ -257,7 +289,7 @@ namespace bertini {
 			variables_.insert( variables_.end(), v.begin(), v.end() );
 		}
 
-		
+
 		/**
 		 Add an implicit parameter to the system.  Implicit parameters are advanced by the tracker akin to variable advancement.
 		 */
@@ -265,8 +297,8 @@ namespace bertini {
 		{
 			implicit_parameters_.push_back(v);
 		}
-		
-		
+
+
 		/**
 		 Add some implicit parameters to the system.  Implicit parameters are advanced by the tracker akin to variable advancement.
 		 */
@@ -274,13 +306,13 @@ namespace bertini {
 		{
 			implicit_parameters_.insert( implicit_parameters_.end(), v.begin(), v.end() );
 		}
-		
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
+
 		/**
 		 Add an explicit parameter to the system.  Explicit parameters should depend only on the path variable, though this is not checked in this function.
 		 */
@@ -288,7 +320,7 @@ namespace bertini {
 		{
 			explicit_parameters_.push_back(F);
 		}
-		
+
 		/**
 		 Add some explicit parameters to the system.  Explicit parameters should depend only on the path variable, though this is not checked in this function.
 		 */
@@ -296,10 +328,10 @@ namespace bertini {
 		{
 			explicit_parameters_.insert( explicit_parameters_.end(), v.begin(), v.end() );
 		}
-		
-		
-		
-		
+
+
+
+
 		/**
 		 Add a subfunction to the system.
 		 */
@@ -307,7 +339,7 @@ namespace bertini {
 		{
 			subfunctions_.push_back(F);
 		}
-		
+
 		/**
 		 Add some subfunctions to the system.
 		 */
@@ -315,9 +347,9 @@ namespace bertini {
 		{
 			subfunctions_.insert( subfunctions_.end(), v.begin(), v.end() );
 		}
-		
-		
-		
+
+
+
 		/**
 		 Add a function to the system.
 		 */
@@ -325,7 +357,7 @@ namespace bertini {
 		{
 			functions_.push_back(F);
 		}
-		
+
 		/**
 		 Add some functions to the system.
 		 */
@@ -333,11 +365,11 @@ namespace bertini {
 		{
 			functions_.insert( functions_.end(), v.begin(), v.end() );
 		}
-		
-		
-		
-		
-		
+
+
+
+
+
 		/**
 		 Add a constant function to the system.  Constants must not depend on anything which can vary -- they're constant!
 		 */
@@ -345,7 +377,7 @@ namespace bertini {
 		{
 			constant_subfunctions_.push_back(F);
 		}
-		
+
 		/**
 		 Add some constant functions to the system.  Constants must not depend on anything which can vary -- they're constant!
 		 */
@@ -353,11 +385,11 @@ namespace bertini {
 		{
 			constant_subfunctions_.insert( constant_subfunctions_.end(), v.begin(), v.end() );
 		}
-		
-		
-		
-		
-		
+
+
+
+
+
 		/**
 		 Add a variable as the Path Variable to a System.  Will overwrite any previously declared path variable.
 		 */
@@ -365,10 +397,10 @@ namespace bertini {
 		{
 			path_variable_ = v;
 		}
-		
-		
-		
-		
+
+
+
+
 		/**
 		 Overloaded operator for printing to an arbirtary out stream.
 		 */
@@ -380,15 +412,15 @@ namespace bertini {
 				out << (iter)->name() << "\n";
 			}
 			out << "\n";
-			
+
 			out << s.NumFunctions() << " functions:\n";
 			for (auto iter : s.functions_) {
 				out << (iter)->name() << "\n";
 				out << *iter << "\n";
 			}
 			out << "\n";
-			
-			
+
+
 			if (s.NumParameters()) {
 				out << s.NumParameters() << " explicit parameters:\n";
 				for (auto iter : s.explicit_parameters_) {
@@ -397,8 +429,8 @@ namespace bertini {
 				}
 				out << "\n";
 			}
-			
-			
+
+
 			if (s.NumConstants()) {
 				out << s.NumConstants() << " constants:\n";
 				for (auto iter : s.constant_subfunctions_) {
@@ -407,60 +439,64 @@ namespace bertini {
 				}
 				out << "\n";
 			}
-			
+
 			if (s.path_variable_) {
 				out << "path variable defined.  named " << s.path_variable_->name() << "\n";
 			}
-			
+
 			return out;
 		}
-        
-        
-        
-        
-        
-        
-        
+
+
+
+
+
+
+
         /////////////// TESTING ////////////////////
         auto function()
         {
             return functions_[0];
         }
-        
+
         auto variables()
         {
             return variables_;
         }
 		/////////////// TESTING ////////////////////
-		
+
 	private:
-		
-		
+
+
 		std::vector<Var> ungrouped_variables_;
 		std::vector<std::vector<Var> > variable_groups_;
 		std::vector<std::vector<Var> > hom_variable_groups_;
-		
-		
+
+
 		std::vector< Fn > functions_;
 		std::vector< Fn > subfunctions_;
 		std::vector< Fn > explicit_parameters_;
-		
-		
+
+
 		std::vector< Var > variables_;
 		std::vector< Var > implicit_parameters_;
-		
-		
+
+
 		Var path_variable_;
-		
+
 		std::vector< Fn > constant_subfunctions_;
-		
+
+		std::vector< Jac > jacobian_;
+		bool is_differentiated_;
+
+
 		unsigned precision_;
-		
-		
+
+
 		// i disagree with the inclusion of this, but the real necessity of it remains to be seen.  --dab
 		Vec<bertini::complex> solutions_;
 	};
-	
+
 }
 
 
