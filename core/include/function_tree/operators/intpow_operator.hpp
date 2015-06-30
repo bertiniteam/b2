7d58c9882c44a9f4d43e8756932f9ce6f19d213f
daniel brake <danielthebrake@gmail.com>
2015-06-29 21:01:23 -0400

    changing the name of the ExponOperator to IntegerPowerOperator, because the purpose is much clearer. renamed the name of the in which it lives.

--- core/include/function_tree/operators/intpow_operator.hpp
+++ core/include/function_tree/operators/intpow_operator.hpp
@@ -16,12 +16,12 @@
 //  Created by Collins, James B. on 4/30/15.
 //
 //
-// exp_operator.hpp:  Declares the class  ExponOperator.
+// exp_operator.hpp:  Declares the class  IntegerPowerOperator.
 
 
 
-#ifndef b2Test_ExponOperator_h
-#define b2Test_ExponOperator_h
+#ifndef b2Test_IntegerPowerOperator_h
+#define b2Test_IntegerPowerOperator_h
 
 #include "function_tree/Node.hpp"
 #include "function_tree/operators/unary_operator.hpp"
@@ -33,13 +33,13 @@ namespace bertini {
 	using std::pow;
 
 	/**
-	 Node -> UnaryOperator -> ExponOperator
+	 Node -> UnaryOperator -> IntegerPowerOperator
 
 	 Description: This class represents the exponentiation operator.  The base is stored in
 	 children_, and an extra variable(exponent_) stores the exponent.  FreshEval is
 	 defined as the exponention operation.
 	 */
-	class ExponOperator : public virtual UnaryOperator
+	class IntegerPowerOperator : public virtual UnaryOperator
 	{
 	public:
 		// These do nothing for a constant
@@ -95,7 +95,7 @@ namespace bertini {
 			}
 	        else{
 	        	auto M = std::make_shared<MultOperator>(std::make_shared<Number>(exponent_), 
-	        	                                        std::make_shared<ExponOperator>(child_, exponent_-1)
+	        	                                        std::make_shared<IntegerPowerOperator>(child_, exponent_-1)
 	        	                                        );
 	        	M->AddChild(child_->Differentiate());
 	        	return M;
@@ -103,17 +103,17 @@ namespace bertini {
         }
 
 
-		virtual ~ExponOperator() = default;
+		virtual ~IntegerPowerOperator() = default;
 
 
 		/**
 		 Constructor, passing in the Node you want as the base, and the integer you want for the power.
 		 */
-		ExponOperator(const std::shared_ptr<Node> & N, int p = 1) : exponent_(p), UnaryOperator(N)
+		IntegerPowerOperator(const std::shared_ptr<Node> & N, int p = 1) : exponent_(p), UnaryOperator(N)
 		{}
 
 
-		ExponOperator(){}
+		IntegerPowerOperator(){}
 
 	protected:
 		// Specific implementation of FreshEval for exponentiate.
@@ -157,14 +157,14 @@ namespace bertini {
 namespace {
 
 	using Node = bertini::Node;
-	using ExponOperator = bertini::ExponOperator;
+	using IntegerPowerOperator = bertini::IntegerPowerOperator;
 
 	/**
 	 Overloading of the power function to create an ExpOperator with base N and power p.
 	 */
 	inline std::shared_ptr<Node> pow(const std::shared_ptr<Node> & base, int power)
 	{
-		return std::make_shared<ExponOperator>(base,power);
+		return std::make_shared<IntegerPowerOperator>(base,power);
 	}
 
 }
