0ea75f0e2fa1727f022a618179d713a60c52ca62
daniel brake <danielthebrake@gmail.com>
2015-07-03 10:50:25 -0400

    added tests for arc trig functions.

--- core/src/function_tree/operators/trig.cpp
+++ core/src/function_tree/operators/trig.cpp
@@ -3,33 +3,8 @@
 
 namespace bertini {
 	
-	
-	std::shared_ptr<Node> SinOperator::Differentiate()
-	{
-		auto ret_mult = std::make_shared<MultOperator>();
-		auto cos_op = std::make_shared<CosOperator>(child_);
-		ret_mult->AddChild(cos_op);
-		ret_mult->AddChild(child_->Differentiate());
-		return ret_mult;
-	}
-	
-	void CosOperator::print(std::ostream & target) const
-	{
-		target << "cos(";
-		child_->print(target);
-		target << ")";
-	}
-	
-	std::shared_ptr<Node> CosOperator::Differentiate()
-	{
-		auto ret_mult = std::make_shared<MultOperator>();
-		std::shared_ptr<Node> sin_op = std::make_shared<SinOperator>(child_);
-		ret_mult->AddChild(sin_op);
-		ret_mult->AddChild(child_->Differentiate());
-		return std::make_shared<NegateOperator>(ret_mult);
-	}
-	
-	int CosOperator::Degree(std::shared_ptr<Variable> const& v) const
+
+	int TrigOperator::Degree(std::shared_ptr<Variable> const& v) const
 	{
 		if (child_->Degree(v)==0)
 		{
@@ -40,12 +15,71 @@ namespace bertini {
 			return -1;
 		}
 	}
+
+
+
+
+	void SinOperator::print(std::ostream & target) const
+	{
+		target << "sin(";
+		child_->print(target);
+		target << ")";
+	}
+
+
+
+	std::shared_ptr<Node> SinOperator::Differentiate()
+	{
+		return cos(child_) * child_->Differentiate();
+	}
 	
+
+
+
+	void ArcSinOperator::print(std::ostream & target) const
+	{
+		target << "asin(";
+		child_->print(target);
+		target << ")";
+	}
+
 
 	std::shared_ptr<Node> ArcSinOperator::Differentiate()
 	{
 		return child_->Differentiate()/sqrt(1-pow(child_,2));
 	}
+
+
+
+
+	void CosOperator::print(std::ostream & target) const
+	{
+		target << "cos(";
+		child_->print(target);
+		target << ")";
+	}
+	
+	std::shared_ptr<Node> CosOperator::Differentiate()
+	{
+		return -sin(child_) * child_->Differentiate();
+	}
+	
+	
+
+
+
+
+
+
+	void ArcCosOperator::print(std::ostream & target) const
+	{
+		target << "acos(";
+		child_->print(target);
+		target << ")";
+	}
+
+
+
 
 	std::shared_ptr<Node> ArcCosOperator::Differentiate()
 	{
@@ -53,12 +87,41 @@ namespace bertini {
 	}
 
 
+
+
+
+
+
+	void TanOperator::print(std::ostream & target) const
+	{
+		target << "tan(";
+		child_->print(target);
+		target << ")";
+	}
+
+
+	std::shared_ptr<Node> TanOperator::Differentiate()
+	{
+		return child_->Differentiate() /  pow(cos(child_),2);
+	}
+
+
+
+
+
+
+	void ArcTanOperator::print(std::ostream & target) const
+	{
+		target << "atan(";
+		child_->print(target);
+		target << ")";
+	}
+		
+
+
 	std::shared_ptr<Node> ArcTanOperator::Differentiate()
 	{
-		auto ret_mult = std::make_shared<MultOperator>();
-		ret_mult->AddChild(child_->Differentiate());
-		ret_mult->AddChild(1 + pow(child_,2), false);
-		return ret_mult;
+		return child_->Differentiate() / (1 + pow(child_,2));
 	}
 
 	
