46989938077205931f0a04131ae63cd6539b5948
daniel brake <danielthebrake@gmail.com>
2015-07-13 16:42:14 -0400

    added copyright and license information to the tops of files, and corrected some typos.

--- core/src/test/classes/start_system_test.cpp
+++ core/src/test/classes/start_system_test.cpp
@@ -1,3 +1,27 @@
+//This file is part of Bertini 2.0.
+//
+//start_system_test.cpp is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//start_system_test.cpp is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with start_system_test.cpp.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+//  start_system_test.cpp
+//
+//  copyright 2015
+//  Daniel Brake
+//  University of Notre Dame
+//  ACMS
+//  Spring, Summer 2015
+
 //start_system_test.cpp
 //
 
