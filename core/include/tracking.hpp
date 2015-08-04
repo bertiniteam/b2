1ea1e79230fca1870298593ca872e42c1a4cea31
daniel brake <danielthebrake@gmail.com>
2015-08-04 11:43:37 +0900

    added copyright header for new files.

---
+++ core/include/tracking.hpp
@@ -0,0 +1,23 @@
+//This file is part of Bertini 2.0.
+//
+//tracking.hpp is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//tracking.hpp is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with tracking.hpp.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+//  tracking.hpp
+//
+//  copyright 2015
+//  Daniel Brake
+//  University of Notre Dame
+//  ACMS
+//  Summer 2015
