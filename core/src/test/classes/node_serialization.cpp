5a05dc4b5d86bfb43d9a6345d080734ab6d49e98
daniel brake <danielthebrake@gmail.com>
2015-08-12 12:59:13 -0400

    added node_serialization.cpp, and link to it for the b2_class_test program

---
+++ core/src/test/classes/node_serialization.cpp
@@ -0,0 +1,83 @@
+//This file is part of Bertini 2.0.
+//
+//node_serialization.cpp is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//node_serialization.cpp is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with node_serialization.cpp.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+//  node_serialization.cpp
+//
+//  Created by Collins, James B. on 4/30/15.
+//  Copyright (c) 2015 West Texas A&M University. All rights reserved.
+//
+// also modified by
+//  Daniel Brake
+//  University of Notre Dame
+//  ACMS
+//  Spring, Summer 2015
+
+
+#include <iostream>
+
+#include <cstdlib>
+#include <cmath>
+
+#include "function_tree.hpp"
+
+
+#include <boost/spirit/include/qi.hpp>
+#include <boost/test/unit_test.hpp>
+
+
+
+using Variable = bertini::Variable;
+using Node = bertini::Node;
+using Float = bertini::Float;
+
+
+
+
+extern double threshold_clearance_d;
+extern unsigned FUNCTION_TREE_TEST_MPFR_DEFAULT_DIGITS;
+extern double threshold_clearance_mp;
+
+
+BOOST_AUTO_TEST_SUITE(node_serialization)
+
+
+BOOST_AUTO_TEST_CASE(serialize_variable)
+{
+
+}
+
+
+BOOST_AUTO_TEST_CASE(serialize_float)
+{
+
+}
+
+BOOST_AUTO_TEST_CASE(serialize_integer)
+{
+
+}
+
+
+BOOST_AUTO_TEST_CASE(serialize_rational)
+{
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
+
+
