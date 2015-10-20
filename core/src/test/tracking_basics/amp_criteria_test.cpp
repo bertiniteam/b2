62a3b3eb03c85cebaafc2d7e689d47494684e518
Tim Hodges <timothodge@gmail.com>
2015-10-20 12:48:01 -0600

    CriterionA test case implemented. Using Griewank and Osborne with high safety digits 1 in AMP configuration.

--- core/src/test/tracking_basics/amp_criteria_test.cpp
+++ core/src/test/tracking_basics/amp_criteria_test.cpp
@@ -59,11 +59,54 @@ extern unsigned TRACKING_TEST_MPFR_DEFAULT_DIGITS;
 BOOST_AUTO_TEST_SUITE(amp_criteria_tracking_basics)
 
 
-//BOOST_AUTO_TEST_CASE(AMP_criteriaA_double)
-//{
-//	BOOST_CHECK_EQUAL("test implemented","true");
-//}
-//	
+BOOST_AUTO_TEST_CASE(AMP_criteriaA_double)
+{
+	/*
+	Using the Griewank Osborne example. Starting at t = 0 where there is a multiplicity 3 isolated solution. We predict 
+	to .1 and try to correct back down. Anywhere except at t = 0, we will have divergence. 
+	Also, saftey_digits_1 has been set to 32000 to set off the AMPCriterionB condition. 
+	*/
+	//Setting upt current space and time values for evaluation
+	Vec<mpfr> current_space(2);
+	current_space << mpfr("256185069753.408853236449242927412","-387520022558.051912233172374487976"),
+					 mpfr("-0.0212298348984663761753389403711889","-0.177814646531698303094367623155171");
+
+	mpfr current_time("0");
+	mpfr delta_t(".1");
+	current_time += delta_t;
+
+	//Defining the system and variables. 
+	bertini::System sys;
+	Var x = std::make_shared<Variable>("x"), y = std::make_shared<Variable>("y"), t = std::make_shared<Variable>("t");
+	VariableGroup vars{x,y};
+
+	sys.AddVariableGroup(vars);
+	sys.AddPathVariable(t);
+	sys.AddFunction(mpfr_float(29/16)*pow(x,3) - 2*x*y + t);
+	sys.AddFunction(y - pow(x,2));
+
+	//For Criterion A to be checked we need Norm_J and inverse of Norm_J these were taken from Euler.hpp
+	Mat<mpfr> dh_dx = sys.Jacobian(current_space, current_time); 
+	auto LU = dh_dx.lu();
+
+	Vec<mpfr> randy = Vec<mpfr>::Random(sys.NumVariables());
+	Vec<mpfr> temp_soln = LU.solve(randy);
+					
+	auto norm_J = dh_dx.norm();
+	auto norm_J_inverse = temp_soln.norm();
+
+
+	//Setting up saftety digits to trigger AMP Criterion A failure.
+	auto AMP = bertini::tracking::config::AMPConfigFrom(sys);
+	AMP.safety_digits_1 = 32000;
+
+	auto CritA = bertini::tracking::amp::CriterionA(norm_J,norm_J_inverse,AMP);
+	std::cout<< CritA << '\n';
+
+	//Check to make sure we failed. 
+	BOOST_CHECK_EQUAL(CritA,false);
+}
+	
 //BOOST_AUTO_TEST_CASE(AMP_criteriaA_mp)
 //{
 //	BOOST_CHECK_EQUAL("test implemented","true");
