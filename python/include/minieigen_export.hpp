abbe29adc948b9bbd0f7daa346d50c3e8b368563
Jeb Collins <jbcolli2@gmail.com>
2016-02-11 13:07:03 -0600

    Set up for Dan

--- python/include/minieigen_export.hpp
+++ python/include/minieigen_export.hpp
@@ -9,6 +9,8 @@
 #ifndef Xcode_b2_minieigen_export_hpp
 #define Xcode_b2_minieigen_export_hpp
 
+#include <bertini2/function_tree.hpp>
+
 #include "../minieigen/src/common.hpp"
 #include "../minieigen/src/expose.hpp"
 
@@ -22,9 +24,16 @@ namespace bertini{
 		{
 			expose_converters(); // in expose-converters.cpp
 			
-			expose_vectors();
-			expose_matrices(); // must come after vectors
-			expose_complex();
+			
+			class_<Eigen::Matrix<dbl,Eigen::Dynamic,1>>("VectorXd","/*TODO*/",
+														 py::init<>()).def(VectorVisitor<Eigen::Matrix<dbl,Eigen::Dynamic,1>>());
+			class_<Eigen::Matrix<mpfr,Eigen::Dynamic,1>>("VectorXmp","/*TODO*/",
+														py::init<>()).def(VectorVisitor<Eigen::Matrix<mpfr,Eigen::Dynamic,1>>());
+			
+			class_<Eigen::Matrix<dbl,Eigen::Dynamic,Eigen::Dynamic>>("MatrixXd","/*TODO*/",
+														py::init<>()).def(VectorVisitor<Eigen::Matrix<dbl,Eigen::Dynamic,Eigen::Dynamic>>());
+			class_<Eigen::Matrix<mpfr,Eigen::Dynamic,Eigen::Dynamic>>("MatrixXmp","/*TODO*/",
+														 py::init<>()).def(VectorVisitor<Eigen::Matrix<mpfr,Eigen::Dynamic,Eigen::Dynamic>>());
 
 		};
 		
