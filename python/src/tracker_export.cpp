239973e6d86eecdc2c579ee397af20814b1c3ede
daniel brake <danielthebrake@gmail.com>
2016-05-16 15:59:33 -0400

    initial exposure of tracker into python.

---
+++ python/src/tracker_export.cpp
@@ -0,0 +1,45 @@
+//This file is part of Bertini 2.
+//
+//python/tracker_export.cpp is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//python/tracker_export.cpp is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with python/tracker_export.cpp.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Copyright(C) 2016 by Bertini2 Development Team
+//
+// See <http://www.gnu.org/licenses/> for a copy of the license, 
+// as well as COPYING.  Bertini2 is provided with permitted 
+// additional terms in the b2/licenses/ directory.
+
+// individual authors of this file include:
+//
+//  Daniel Brake
+//  University of Notre Dame
+//  Summer 2016
+//
+//
+//  python/tracker_export.cpp:  source file for exposing trackers to python.
+
+#include "tracker.hpp"
+
+namespace bertini{
+	namespace python{
+
+		template<typename TrackerT>
+		template<class PyClass>
+		void TrackerVisitor<TrackerT>::visit(PyClass& cl) const
+		{
+			cl
+			.def("Setup", &TrackerT::Setup)
+			.def("TrackPath", &TrackerT::TrackPath);
+		}
+}} // namespaces
+
