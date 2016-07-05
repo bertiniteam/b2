55b7e30a0f1400db262f4a421b75f0284a1eb8a8
daniel brake <danielthebrake@gmail.com>
2016-07-05 17:17:49 -0400

    corrected missing config.hpp file, was erroneously ignored.

---
+++ core/include/bertini2/nag_algorithms/config.hpp
@@ -0,0 +1,81 @@
+//This file is part of Bertini 2.
+//
+//nag_algorithms/config.hpp is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//nag_algorithms/config.hpp is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with nag_algorithms/config.hpp.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Copyright(C) 2015, 2016 by Bertini2 Development Team
+//
+// See <http://www.gnu.org/licenses/> for a copy of the license, 
+// as well as COPYING.  Bertini2 is provided with permitted 
+// additional terms in the b2/licenses/ directory.
+
+// individual authors of this file include:
+// daniel brake, university of notre dame
+// Tim Hodges, Colorado State University
+ 
+#pragma once
+
+namespace bertini{
+	namespace algorithm{
+		namespace config{
+
+
+template<typename T>
+struct Tolerances
+{	
+	T newton_before_endgame = T(1)/T(100000);
+	T newton_during_endgame = T(1)/T(1000000);
+
+	T final_tolerance = T(1)/T(100000000000);
+	T final_tolerance_multiplier = T(10); // This multiplier is used to cluster or de-cluster points at the target system. 
+
+	T path_truncation_threshold = T(100000);
+	T final_tolerance_times_final_tolerance_multiplier = final_tolerance * final_tolerance_multiplier;
+};
+
+template<typename T>
+struct Sharpening
+{
+	unsigned sharpendigits; ///< how many digits should be correct after sharpening.
+	
+	std::function<Vec<T>> sharpen_method_; ///< function taking a vector, and sharpening it.
+
+	T function_residual_tolerance; ///<
+	T ratio_tolerance; ///<
+};
+
+template<typename T>
+struct Regeneration
+{
+	bool remove_infinite_endpoints = true; ///<  Bool indicating whether endpoints during the regeneration start point buildup step which are infinite should be discarded.  If you are not interested in infinite solutions, ensure this is true.
+
+	bool higher_dimension_check;
+
+	T newton_before_endgame; ///< The tolerance for tracking before reaching the endgame.
+	T newton_during_endgame; ///< The tolerance for tracking during the endgame.
+	T final_tolerance; ///< The final tolerance to track to, using the endgame.
+};
+
+
+template<typename T>
+struct PostProcessing{
+	T real_threshold; ///< threshold on the imaginary part of a solution being 0.  If the imag part exceeds this, the point is considered complex.  Currently, this is the implemented available way in Bertini2 for determining this, but there are other methods.  Smale's alpha theory provides ways to prove that a point is real.  If this is something you need, please consider adding the method to the library, for all to use!  Or, if this is technically beyond your C++ capabilities, add as an issue on the github page, and indicate it as a feature request.
+
+	T endpoint_finite_threshold;  ///< The threshold on norm of endpoints being considered infinite.  There is another setting in Tolerances, `path_truncation_threshold`, which tells the path tracker to die if exceeded.  Another related setting is in Security, `max_norm` -- the endgame dies if the norm of the computed approximation exceeds this twice.
+
+	T final_tol_multiplier; ///< A mystery
+	T final_tol_times_mult; ///< Another mystery
+};
+		}
+	}
+}
