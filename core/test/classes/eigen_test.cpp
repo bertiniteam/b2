95799a1e8d3ffde10255fb362ee4daa97df34c6c
dani brake <danielthebrake@gmail.com>
2016-09-20 12:13:26 -0400

    added a test for whether the NumTraits we provide is actually used.
    Our NumTraits for expressions of mpfr_floats was not written correctly, so the GenericNumTraits was used, causing the ::Real type to be incorrect.  namely, it was producing the self-same expression as the Real type, which is not instantiable.  Fix for NumTraits coming in a subsequent commit.

--- core/test/classes/eigen_test.cpp
+++ core/test/classes/eigen_test.cpp
@@ -49,7 +49,21 @@ using bertini::KahanMatrix;
 
 
 
+BOOST_AUTO_TEST_SUITE(num_traits_checking)
 
+
+// this test assures that the Eigen::NumTraits defined in eigen_extensions.hpp is correctly found during template instantiation, and that the Real type it defines is actually mpfr_float.  If it were not, then we would be unable to make an expression of ::Real type in variable q, because it would be an expression, not a populatable number of type mpfr_float.
+BOOST_AUTO_TEST_CASE(expressions_of_mpfr_floats)
+{
+	using NumT = bertini::mpfr_float;
+
+	NumT a {1}, b{2}, c{3};
+
+	Eigen::NumTraits<decltype(a*a + b*b/ c)>::Real q{0};
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()
 
 
 BOOST_AUTO_TEST_SUITE(kahan_matrix_solving_LU)
