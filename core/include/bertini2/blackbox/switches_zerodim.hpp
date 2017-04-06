e4fa2c97d55bf126a5336da1007677635586f44c
dani brake <danielthebrake@gmail.com>
2017-04-06 11:24:47 -0400

    added license/copyright to switches/zerodim.hpp

--- core/include/bertini2/blackbox/switches_zerodim.hpp
+++ core/include/bertini2/blackbox/switches_zerodim.hpp
@@ -1,3 +1,33 @@
+//This file is part of Bertini 2.
+//
+//bertini2/blackbox/switches_zerodim.hpp is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//bertini2/blackbox/switches_zerodim.hpp is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with bertini2/blackbox/switches_zerodim.hpp.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Copyright(C) 2015, 2016 by Bertini2 Development Team
+//
+// See <http://www.gnu.org/licenses/> for a copy of the license, 
+// as well as COPYING.  Bertini2 is provided with permitted 
+// additional terms in the b2/licenses/ directory.
+
+
+/**
+\file bertini2/blackbox/switches_zerodim.hpp 
+
+\brief A sequence of switches for getting a particular instantiation of a ZeroDim algorithm, based on runtime options.
+*/
+
+
+
 #pragma once
 
 
