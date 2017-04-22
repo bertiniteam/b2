a9aa250d02c351ce85cba92a802dbee11464c876
Tim Hodges <timothodge@gmail.com>
2017-04-22 09:47:33 -0600

    Added the following test cases for multi-homogeneous start systems. 1. 4 functions, 4 variables, with 2 variable groups. 2. 4 functions, 4 variables, with 4 variable groups. 3. 4 functions, 4 variables, with 1 variable group. 4. Zero column in degree matrix. 5. Diagonal degree matrix. 6. Zero row in degree matrix. 7. non-square target system 8. empty variable group. 9. ungrouped variable for target system. 10. variable in many variable groups.

--- core/test/classes/m_hom_start_system.cpp
+++ core/test/classes/m_hom_start_system.cpp
@@ -139,10 +139,6 @@ BOOST_AUTO_TEST_CASE(m_hom_system_preliminary_construction_larger_example)
 
 	auto mhom_start_system = bertini::start_system::MHomogeneous(sys);
 
-	// for(int ii = 0; ii < mhom_start_system.valid_partitions.size(); ii++)
-	// {
-	// 	std::cout << "valid partition: " << std::endl <<  mhom_start_system.valid_partitions[ii] << std::endl; 
-	// }
 
 	Vec<int> partition_1(3);
 	partition_1 << 0, 1,2;
@@ -166,6 +162,381 @@ BOOST_AUTO_TEST_CASE(m_hom_system_preliminary_construction_larger_example)
 	BOOST_CHECK(mhom_start_system.degree_matrix_(2,2) == 3);
 
 	BOOST_CHECK(mhom_start_system.NumStartPoints() == 12);
+
+}
+
+BOOST_AUTO_TEST_CASE(Two_var_groups_4_vars_4_fctns_example)
+{
+	//	Basic problem from Dan Bates 
+	//  	variable groups {x1,x2}, {x3,x4}
+	// 		f1 = x1^2 + x4
+	//  	f2 = x1*x2*x3
+	//  	f3 = x4^2 
+	//  	f4 = x3^2
+	//  	Degree matrix 
+	//  	[2 1]
+	//		[2 1]
+	//		[0 2]
+	//		[0 2]
+	//		Number of paths: 16
+ 	auto x1 = MakeVariable("x1");
+	auto x2 = MakeVariable("x2");
+	auto x3 = MakeVariable("x3");
+	auto x4 = MakeVariable("x4");
+
+
+	System sys;
+
+	VariableGroup v1{x1,x2};
+	VariableGroup v2{x3,x4};
+
+	sys.AddVariableGroup(v1);
+	sys.AddVariableGroup(v2);
+
+	sys.AddFunction(pow(x1,2) + x4);
+	sys.AddFunction(x1*x2*x3);
+	sys.AddFunction(pow(x4,2));
+	sys.AddFunction(pow(x3,2));
+
+	auto mhom_start_system = bertini::start_system::MHomogeneous(sys);
+
+	BOOST_CHECK(mhom_start_system.degree_matrix_(0,0) == 2);
+	BOOST_CHECK(mhom_start_system.degree_matrix_(1,0) == 2);
+	BOOST_CHECK(mhom_start_system.degree_matrix_(2,0) == 0);
+	BOOST_CHECK(mhom_start_system.degree_matrix_(3,0) == 0);
+
+	BOOST_CHECK(mhom_start_system.degree_matrix_(0,1) == 1);
+	BOOST_CHECK(mhom_start_system.degree_matrix_(1,1) == 1);
+	BOOST_CHECK(mhom_start_system.degree_matrix_(2,1) == 2);
+	BOOST_CHECK(mhom_start_system.degree_matrix_(3,1) == 2);
+
+	BOOST_CHECK(mhom_start_system.NumStartPoints() == 16);
+
+}
+
+BOOST_AUTO_TEST_CASE(four_var_groups_4_vars_4_fctns_example)
+{
+	//	Basic problem from Dan Bates 
+	//  	variable groups {x1}, {x2}, {x3}, {x4}
+	// 		f1 = x1^2 + x4
+	//  	f2 = x1*x2*x3
+	//  	f3 = x4^2 
+	//  	f4 = x3^2
+	//  	Degree matrix 
+	//  	[2 0 0 1]
+	//		[1 1 1 0]
+	//		[0 0 0 2]
+	//		[0 0 2 0]
+	//		Number of paths: 8
+	auto x1 = MakeVariable("x1");
+	auto x2 = MakeVariable("x2");
+	auto x3 = MakeVariable("x3");
+	auto x4 = MakeVariable("x4");
+
+
+	System sys;
+
+	VariableGroup v1{x1};
+	VariableGroup v2{x2};
+	VariableGroup v3{x3};
+	VariableGroup v4{x4};
+
+	sys.AddVariableGroup(v1);
+	sys.AddVariableGroup(v2);
+	sys.AddVariableGroup(v3);
+	sys.AddVariableGroup(v4);
+
+	sys.AddFunction(pow(x1,2) + x4);
+	sys.AddFunction(x1*x2*x3);
+	sys.AddFunction(pow(x4,2));
+	sys.AddFunction(pow(x3,2));
+
+	auto mhom_start_system = bertini::start_system::MHomogeneous(sys);
+
+	BOOST_CHECK(mhom_start_system.degree_matrix_(0,0) == 2);
+	BOOST_CHECK(mhom_start_system.degree_matrix_(1,0) == 1);
+	BOOST_CHECK(mhom_start_system.degree_matrix_(2,0) == 0);
+	BOOST_CHECK(mhom_start_system.degree_matrix_(3,0) == 0);
+
+	BOOST_CHECK(mhom_start_system.degree_matrix_(0,1) == 0);
+	BOOST_CHECK(mhom_start_system.degree_matrix_(1,1) == 1);
+	BOOST_CHECK(mhom_start_system.degree_matrix_(2,1) == 0);
+	BOOST_CHECK(mhom_start_system.degree_matrix_(3,1) == 0);
+
+	BOOST_CHECK(mhom_start_system.degree_matrix_(0,2) == 0);
+	BOOST_CHECK(mhom_start_system.degree_matrix_(1,2) == 1);
+	BOOST_CHECK(mhom_start_system.degree_matrix_(2,2) == 0);
+	BOOST_CHECK(mhom_start_system.degree_matrix_(3,2) == 2);
+
+	BOOST_CHECK(mhom_start_system.degree_matrix_(0,3) == 1);
+	BOOST_CHECK(mhom_start_system.degree_matrix_(1,3) == 0);
+	BOOST_CHECK(mhom_start_system.degree_matrix_(2,3) == 2);
+	BOOST_CHECK(mhom_start_system.degree_matrix_(3,3) == 0);
+
+	BOOST_CHECK(mhom_start_system.NumStartPoints() == 8);
+
+}
+
+BOOST_AUTO_TEST_CASE(one_var_group_4_vars_4_fctns_example)
+{
+	//	Basic problem from Dan Bates 
+	//  	variable groups {x1,x2,x3,x4}
+	// 		f1 = x1^2 + x4
+	//  	f2 = x1*x2*x3
+	//  	f3 = x4^2 
+	//  	f4 = x3^2
+	//  	Degree matrix 
+	//  	[2]
+	//		[3]
+	//		[2]
+	//		[2]
+	//		Number of paths: 24
+	auto x1 = MakeVariable("x1");
+	auto x2 = MakeVariable("x2");
+	auto x3 = MakeVariable("x3");
+	auto x4 = MakeVariable("x4");
+
+
+	System sys;
+
+	VariableGroup v1{x1,x2,x3,x4};
+
+	sys.AddVariableGroup(v1);
+
+	sys.AddFunction(pow(x1,2) + x4);
+	sys.AddFunction(x1*x2*x3);
+	sys.AddFunction(pow(x4,2));
+	sys.AddFunction(pow(x3,2));
+
+	auto mhom_start_system = bertini::start_system::MHomogeneous(sys);
+
+	BOOST_CHECK(mhom_start_system.degree_matrix_(0,0) == 2);
+	BOOST_CHECK(mhom_start_system.degree_matrix_(1,0) == 3);
+	BOOST_CHECK(mhom_start_system.degree_matrix_(2,0) == 2);
+	BOOST_CHECK(mhom_start_system.degree_matrix_(3,0) == 2);
+
+
+	BOOST_CHECK(mhom_start_system.NumStartPoints() == 24);
+
+}
+
+
+BOOST_AUTO_TEST_CASE(zero_column_in_degree_matrix)
+{
+	//	Basic problem from Dan Bates 
+	//  	variable groups {x1,x2},{x3}
+	// 		f1 = x1 + x2
+	//  	f2 = x1
+	//  	f3 = x2
+	//  	Degree matrix 
+	//  	[1 0]
+	//		[1 0]
+	//		[1 0]
+	//		Should throw for having a zero column.
+	auto x1 = MakeVariable("x1");
+	auto x2 = MakeVariable("x2");
+	auto x3 = MakeVariable("x3");
+
+
+
+	System sys;
+
+	VariableGroup v1{x1,x2};
+	VariableGroup v2{x3};
+
+
+	sys.AddVariableGroup(v1);
+	sys.AddVariableGroup(v2);
+
+
+	sys.AddFunction(x1 + x2);
+	sys.AddFunction(x1);
+	sys.AddFunction(x2);
+
+	BOOST_CHECK_THROW(auto mhom_start_system = bertini::start_system::MHomogeneous(sys), std::runtime_error);
+
+}
+
+BOOST_AUTO_TEST_CASE(diagonal_degree_matrix)
+{
+	//	Basic problem from Dan Bates 
+	//  	variable groups {x1},{x2},{x3}
+	// 		f1 = x1^2
+	//  	f2 = x2^2
+	//  	f3 = x3^2
+	//  	Degree matrix 
+	//  	[2 0 0]
+	//		[0 2 0]
+	//		[0 0 2]
+	//		Number of paths: 8
+	auto x1 = MakeVariable("x1");
+	auto x2 = MakeVariable("x2");
+	auto x3 = MakeVariable("x3");
+
+	System sys;
+
+	VariableGroup v1{x1};
+	VariableGroup v2{x2};
+	VariableGroup v3{x3};
+
+
+	sys.AddVariableGroup(v1);
+	sys.AddVariableGroup(v2);
+	sys.AddVariableGroup(v3);
+
+	sys.AddFunction(pow(x1,2));
+	sys.AddFunction(pow(x2,2));
+	sys.AddFunction(pow(x3,2));
+
+
+	auto mhom_start_system = bertini::start_system::MHomogeneous(sys);
+
+	BOOST_CHECK(mhom_start_system.degree_matrix_(0,0) == 2);
+	BOOST_CHECK(mhom_start_system.degree_matrix_(1,0) == 0);
+	BOOST_CHECK(mhom_start_system.degree_matrix_(2,0) == 0);
+
+	BOOST_CHECK(mhom_start_system.degree_matrix_(0,1) == 0);
+	BOOST_CHECK(mhom_start_system.degree_matrix_(1,1) == 2);
+	BOOST_CHECK(mhom_start_system.degree_matrix_(2,1) == 0);
+
+	BOOST_CHECK(mhom_start_system.degree_matrix_(0,2) == 0);
+	BOOST_CHECK(mhom_start_system.degree_matrix_(1,2) == 0);
+	BOOST_CHECK(mhom_start_system.degree_matrix_(2,2) == 2);
+
+	BOOST_CHECK(mhom_start_system.NumStartPoints() == 8);
+
+}
+
+
+BOOST_AUTO_TEST_CASE(zero_row_in_degree_matrix)
+{
+	//	Basic problem from Dan Bates 
+	//  	variable groups {x1},{x2}
+	// 		f1 = x1^2 + x2^2
+	//  	f2 = 1
+	//  	Degree matrix 
+	//  	[2 2]
+	//		[0 0]
+	//		Should throw for having a zero row.
+	auto x1 = MakeVariable("x1");
+	auto x2 = MakeVariable("x2");
+
+	System sys;
+
+	VariableGroup v1{x1};
+	VariableGroup v2{x2};
+
+
+	sys.AddVariableGroup(v1);
+	sys.AddVariableGroup(v2);
+
+	sys.AddFunction(pow(x1,2) + pow(x2,2));
+	sys.AddFunction(bertini::MakeInteger(1));
+
+	BOOST_CHECK_THROW(auto mhom_start_system = bertini::start_system::MHomogeneous(sys), std::runtime_error);
+
+}
+
+
+BOOST_AUTO_TEST_CASE(non_square_target_system_for_mhom)
+{
+	//	Basic problem from Dan Bates 
+	//  	variable groups {x}
+	// 		f1 = x^2
+	//  	f2 = x^2 - 1
+	// Will throw for not being square
+	auto x = MakeVariable("x");
+
+
+	System sys;
+
+	VariableGroup v1{x};
+
+	sys.AddVariableGroup(v1);
+
+
+	sys.AddFunction(pow(x,2));
+	sys.AddFunction(pow(x,2) - 1);
+
+	BOOST_CHECK_THROW(auto mhom_start_system = bertini::start_system::MHomogeneous(sys), std::runtime_error);
+
+}
+
+
+BOOST_AUTO_TEST_CASE(empty_variable_group)
+{
+	//	Basic problem from Dan Bates 
+	//  	variable groups {x,y},{}
+	// 		f1 = x^2
+	//  	f2 = y^2 - 1
+	// Should throw for having an empty variable group. 
+	auto x = MakeVariable("x");
+	auto y = MakeVariable("y");
+
+	System sys;
+
+	VariableGroup v1{x,y};
+	VariableGroup v2{};
+
+	sys.AddVariableGroup(v1);
+	sys.AddVariableGroup(v2);
+
+
+	sys.AddFunction(pow(x,2));
+	sys.AddFunction(pow(y,2) - 1);
+
+	BOOST_CHECK_THROW(auto mhom_start_system = bertini::start_system::MHomogeneous(sys), std::runtime_error);
+
+}
+
+
+BOOST_AUTO_TEST_CASE(ungrouped_variable_for_target_system_in_mhom_construction)
+{
+	//	Basic problem from Dan Bates 
+	//  	variable groups {x}, y ungrouped
+	// 		f1 = x^2
+	//  	f2 = y^2 - 1
+	// Should throw for having an ungrouped variable.
+	auto x = MakeVariable("x");
+	auto y = MakeVariable("y");
+
+	System sys;
+
+	VariableGroup v1{x};
+
+	sys.AddVariableGroup(v1);
+
+
+	sys.AddFunction(pow(x,2));
+	sys.AddFunction(pow(y,2) - 1);
+
+	BOOST_CHECK_THROW(auto mhom_start_system = bertini::start_system::MHomogeneous(sys), std::runtime_error);
+
+}
+
+BOOST_AUTO_TEST_CASE(variable_in_many_variable_groups_in_mhom_construction)
+{
+	//	Basic problem from Dan Bates 
+	//  	variable groups {x,y},{y} 
+	// 		f1 = x^2
+	//  	f2 = y^2 - 1
+	// Should throw for having the variable y in many variable groups. 
+	auto x = MakeVariable("x");
+	auto y = MakeVariable("y");
+
+	System sys;
+
+	VariableGroup v1{x,y};
+	VariableGroup v2{y};
+
+	sys.AddVariableGroup(v1);
+	sys.AddVariableGroup(v2);
+
+
+	sys.AddFunction(pow(x,2));
+	sys.AddFunction(pow(y,2) - 1);
+
+	BOOST_CHECK_THROW(auto mhom_start_system = bertini::start_system::MHomogeneous(sys), std::runtime_error);
 
 }
 
