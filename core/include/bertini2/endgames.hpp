4dd76f2e886add58e9d13b4eabc3352ab234f5f8
dani brake <danielthebrake@gmail.com>
2017-08-11 14:42:50 -0500

    simplified the root `endgames.hpp` include

--- core/include/bertini2/endgames.hpp
+++ core/include/bertini2/endgames.hpp
@@ -31,10 +31,11 @@
 
 #pragma once
 
-#include "bertini2/endgames/fixed_prec_cauchy.hpp"
-#include "bertini2/endgames/fixed_prec_powerseries.hpp"
-#include "bertini2/endgames/amp_cauchy.hpp"
-#include "bertini2/endgames/amp_powerseries.hpp"
+#include "bertini2/endgames/config.hpp"
+
+#include "bertini2/endgames/powerseries.hpp"
+// #include "bertini2/endgames/cauchy.hpp"
+
 
 
 
