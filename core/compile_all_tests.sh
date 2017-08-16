74ee1d84dd2866ac2bfabe3c05d798dfa2335648
dani brake <danielthebrake@gmail.com>
2017-08-15 21:34:55 -0500

    improved by replacing echo with printf, and added run_all_tests.sh

--- core/compile_all_tests.sh
+++ core/compile_all_tests.sh
@@ -7,8 +7,8 @@ else
 	numprocs=$1
 fi
 
-echo "compiling with" $numprocs "processor(s)" | tee -a "compile_all.log"
+printf "\n\n\ncompiling with %d processor(s)\n\n\n" $numprocs | tee -a "compile_all.log"
 while read suite; do
-	echo $suite | tee -a "compile_all.log"
+	printf "\n\n%s\n\n" $suite | tee -a "compile_all.log"
 	make -j $numprocs $suite | tee -a "compile_all.log"
 done <test/available_tests.txt
