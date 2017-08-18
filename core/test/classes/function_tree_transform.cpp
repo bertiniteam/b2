f999db280fd003a784e4650b956f2e7c174184ec
dani brake <danielthebrake@gmail.com>
2017-08-18 17:43:35 -0500

    actually compiling function_tree_transform.cpp, and also added several more tests
    some tests fail in this iteration

--- core/test/classes/function_tree_transform.cpp
+++ core/test/classes/function_tree_transform.cpp
@@ -1,35 +1,29 @@
 //This file is part of Bertini 2.
 //
-//function_tree_test.cpp is free software: you can redistribute it and/or modify
+//b2/core/test/classes/function_tree_transform.cpp is free software: you can redistribute it and/or modify
 //it under the terms of the GNU General Public License as published by
 //the Free Software Foundation, either version 3 of the License, or
 //(at your option) any later version.
 //
-//function_tree_test.cpp is distributed in the hope that it will be useful,
+//b2/core/test/classes/function_tree_transform.cpp is distributed in the hope that it will be useful,
 //but WITHOUT ANY WARRANTY; without even the implied warranty of
 //MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //GNU General Public License for more details.
 //
 //You should have received a copy of the GNU General Public License
-//along with function_tree_test.cpp.  If not, see <http://www.gnu.org/licenses/>.
+//along with b2/core/test/classes/function_tree_transform.cpp.  If not, see <http://www.gnu.org/licenses/>.
 //
-// Copyright(C) 2015, 2016 by Bertini2 Development Team
+// Copyright(C) 2017 by Bertini2 Development Team
 //
 // See <http://www.gnu.org/licenses/> for a copy of the license, 
 // as well as COPYING.  Bertini2 is provided with permitted 
 // additional terms in the b2/licenses/ directory.
 
 // individual authors of this file include:
-// daniel brake, university of notre dame
-
-//  Created by Collins, James B. on 4/30/15.
-//  Copyright (c) 2015 West Texas A&M University. All rights reserved.
-//
-// also modified by
-//  Daniel Brake
-//  University of Notre Dame
+//  Dani Brake
+//  University of Wisconsin Eau Claire
 //  ACMS
-//  Spring, Summer 2015
+//  Fall 2017
 
 #include <iostream>
 
@@ -59,13 +53,58 @@ BOOST_AUTO_TEST_SUITE(eliminate_zeros)
 
 BOOST_AUTO_TEST_CASE(cant_eliminate_self)
 {
-	auto z = Nd(MakeInteger(0));
+	auto zero = Nd(MakeInteger(0));
 
-	auto num_eliminated = z.EliminateZeros();
+	auto num_eliminated = zero->EliminateZeros();
 
 	BOOST_CHECK_EQUAL(num_eliminated, 0);
 }
 
+BOOST_AUTO_TEST_CASE(cant_eliminate_self2)
+{
+	auto zero = Nd(MakeInteger(0));
+	auto n = zero;
+
+	auto num_eliminated = n->EliminateZeros();
+
+	BOOST_CHECK_EQUAL(num_eliminated, 0);
+}
+
+
+BOOST_AUTO_TEST_CASE(eliminate_single_zero_sum)
+{
+	auto zero = Nd(MakeInteger(0));
+
+	auto n = zero+zero;
+
+	auto num_eliminated = n->EliminateZeros();
+
+	BOOST_CHECK_EQUAL(num_eliminated, 1);
+}
+
+
+BOOST_AUTO_TEST_CASE(eliminate_single_zero_sum2)
+{
+	auto zero = Nd(MakeInteger(0));
+
+	auto n = zero+0;
+
+	auto num_eliminated = n->EliminateZeros();
+
+	BOOST_CHECK_EQUAL(num_eliminated, 1);
+}
+
+
+BOOST_AUTO_TEST_CASE(eliminate_single_zero_sum3)
+{
+	auto zero = Nd(MakeInteger(0));
+
+	auto n = 0+zero;
+
+	auto num_eliminated = n->EliminateZeros();
+
+	BOOST_CHECK_EQUAL(num_eliminated, 1);
+}
 
 BOOST_AUTO_TEST_SUITE_END()
 
