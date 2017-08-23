96c806b704b5d615336c993c98e09a97ff2ec165
dani brake <danielthebrake@gmail.com>
2017-08-23 15:31:20 -0500

    added two more setters for variables -- for randoms, random units.  also, now not NaN when making a variable, but instead a random number

--- core/test/classes/function_tree_test.cpp
+++ core/test/classes/function_tree_test.cpp
@@ -121,15 +121,15 @@ BOOST_AUTO_TEST_CASE(manual_construction_x_squared){
 
 
 
-BOOST_AUTO_TEST_CASE(default_constructed_variable_is_nan){
+BOOST_AUTO_TEST_CASE(default_constructed_variable_is_not_nan){
 	using mpfr_float = bertini::mpfr_float;
 	bertini::DefaultPrecision(CLASS_TEST_MPFR_DEFAULT_DIGITS);
 	
 	std::shared_ptr<Variable> x = MakeVariable("x");
 	
 	using bertini::isnan;
-	BOOST_CHECK(isnan(x->Eval<dbl>()));
-	BOOST_CHECK(isnan(x->Eval<mpfr>()));
+	BOOST_CHECK(!isnan(x->Eval<dbl>()));
+	BOOST_CHECK(!isnan(x->Eval<mpfr>()));
 }
 
 
