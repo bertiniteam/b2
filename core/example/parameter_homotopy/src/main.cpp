bff1519fc805a0f9fef7e97576aec38a9c01ae58
dani brake <danielthebrake@gmail.com>
2017-08-30 14:28:47 -0500

    cleanup of source for parameter homotopy example

--- core/example/parameter_homotopy/src/main.cpp
+++ core/example/parameter_homotopy/src/main.cpp
@@ -8,13 +8,9 @@ int main()
     bertini::LoggingInit();
 
 
-    auto t = bertini::MakeVariable("t");
+    
 
     auto step1_params = demo::MakeStep1Parameters();
-
-    auto step2_stuff = demo::MakeStep2Parameters(step1_params, t);
-
-
     auto target_sys_step1 = demo::ConstructSystem(step1_params);
 
     std::cout << "your target_system for step 1:\n\n";
@@ -28,6 +24,8 @@ int main()
         std::cout << iter << '\n';
 
 
+    auto t = bertini::MakeVariable("t");
+    auto step2_stuff = demo::MakeStep2Parameters(step1_params, t);
     auto homotopy_sys_step2 = demo::ConstructSystem(std::get<0>(step2_stuff));
     homotopy_sys_step2.AddPathVariable(t);
     auto target_sys_step2 = demo::ConstructSystem(std::get<1>(step2_stuff));
@@ -48,11 +46,10 @@ int main()
     bertini::DefaultPrecision(16);
 
     std::cout << "your target system for step2:\n" << target_sys_step2 << '\n';
-
-
-
     for (auto& p : std::get<1>(step2_stuff))
         std::cout << "solving for parameter values " << *p <<  " " << p->Eval<bertini::dbl>() << '\n';
+
+
 
     auto steptwo_solutions = demo::StepTwo(target_sys_step2, target_sys_step1, homotopy_sys_step2, stepone_solutions);
     
