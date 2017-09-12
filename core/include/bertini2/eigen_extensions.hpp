b698457437613b06de370f114a9153862f9f2db3
dani brake <brakeda@uwec.edu>
2017-09-12 19:23:14 -0400

    added digits10() implementation for NumTraits for mpfr_float, and consequently the bertini::complex
    a compile error was happening in GCC5.4, eigen 3.3.4 because couldn't convert from ceil(mpfr_float) to int.  this is due to a bug in Boost.Multiprecision in versions 1.58.  It was fixed at some point.

--- core/include/bertini2/eigen_extensions.hpp
+++ core/include/bertini2/eigen_extensions.hpp
@@ -15,15 +15,15 @@
 //
 // Copyright(C) 2015 - 2017 by Bertini2 Development Team
 //
-// See <http://www.gnu.org/licenses/> for a copy of the license, 
-// as well as COPYING.  Bertini2 is provided with permitted 
+// See <http://www.gnu.org/licenses/> for a copy of the license,
+// as well as COPYING.  Bertini2 is provided with permitted
 // additional terms in the b2/licenses/ directory.
 
 // individual authors of this file include:
 // dani brake, University of Wisconsin Eau Claire
 
 /**
-\file eigen_extensions.hpp 
+\file eigen_extensions.hpp
 
 \brief Bertini extensions of the Eigen linear algebra library.
 */
@@ -42,11 +42,11 @@
 #include <Eigen/Core>
 
 namespace Eigen {
-	
+
 	using mpfr_float = bertini::mpfr_float;
 	template<> struct NumTraits<mpfr_float> : GenericNumTraits<mpfr_float> // permits to get the epsilon, dummy_precision, lowest, highest functions
 	{
-		
+
 		typedef mpfr_float Real;
 		typedef mpfr_float NonInteger;
 		typedef mpfr_float Nested;
@@ -59,39 +59,45 @@ namespace Eigen {
 			AddCost = 30,
 			MulCost = 40
 		};
-		
-		
+
+
 		inline static Real highest() {
-			
+
 			return (mpfr_float(1) - epsilon()) * pow(mpfr_float(2),mpfr_get_emax()-1);//);//DefaultPrecision());
 		}
-		
+
 		inline static Real lowest() {
 			return -highest();
 		}
-		
+
 		inline static Real dummy_precision()
 		{
 			using bertini::DefaultPrecision;
 			return pow( mpfr_float(10),-int(DefaultPrecision()-3));
 		}
-		
+
 		inline static Real epsilon()
 		{
 			using bertini::DefaultPrecision;
 			return pow(mpfr_float(10),-int(DefaultPrecision()));
 		}
+
+		static inline int digits10()
+		{
+			return bertini::DefaultPrecision();
+			// return internal::default_digits10_impl<T>::run();
+		}
 		//http://www.manpagez.com/info/mpfr/mpfr-2.3.2/mpfr_31.php
 	};
 
-	
-	template<typename Expr1,typename Expr2, typename Expr3> 
+
+	template<typename Expr1,typename Expr2, typename Expr3>
 	struct NumTraits<
 		boost::multiprecision::detail::expression<Expr1,
       	Expr2, Expr3, void, void>
       		> : NumTraits<mpfr_float> // permits to get the epsilon, dummy_precision, lowest, highest functions
 	{
-		
+
 		typedef mpfr_float Real;
 		typedef mpfr_float NonInteger;
 		typedef mpfr_float Nested;
@@ -106,7 +112,7 @@ namespace Eigen {
 
 	 Provides methods to get the epsilon, dummy_precision, lowest, highest functions, largely by inheritance from the NumTraits<mpfr_float> contained in mpfr_extensions.
 	 */
-	template<> struct NumTraits<bertini::complex> : NumTraits<bertini::mpfr_float> 
+	template<> struct NumTraits<bertini::complex> : NumTraits<bertini::mpfr_float>
 	{
 		using mpfr_float = bertini::mpfr_float;
 
@@ -122,7 +128,7 @@ namespace Eigen {
 			AddCost = 2 * NumTraits<Real>::AddCost,
 			MulCost = 4 * NumTraits<Real>::MulCost + 2 * NumTraits<Real>::AddCost
 		};
-		
+
 	};
 
 	namespace internal {
@@ -171,45 +177,45 @@ namespace Eigen {
 		// see https://forum.kde.org/viewtopic.php?f=74&t=111176
 
 		//int
-		template<> 
-		struct scalar_product_traits<int,bertini::complex> 
+		template<>
+		struct scalar_product_traits<int,bertini::complex>
 		{
 	    	enum { Defined = 1 };
 	    	typedef bertini::complex ReturnType;
 		};
 
-		template<> 
-		struct scalar_product_traits<bertini::complex, int> 
+		template<>
+		struct scalar_product_traits<bertini::complex, int>
 		{
 	    	enum { Defined = 1 };
 	    	typedef bertini::complex ReturnType;
 		};
 
 		//long
-		template<> 
-		struct scalar_product_traits<long,bertini::complex> 
+		template<>
+		struct scalar_product_traits<long,bertini::complex>
 		{
 	    	enum { Defined = 1 };
 	    	typedef bertini::complex ReturnType;
 		};
 
-		template<> 
-		struct scalar_product_traits<bertini::complex, long> 
+		template<>
+		struct scalar_product_traits<bertini::complex, long>
 		{
 	    	enum { Defined = 1 };
 	    	typedef bertini::complex ReturnType;
 		};
 
 		//long long
-		template<> 
-		struct scalar_product_traits<long long,bertini::complex> 
+		template<>
+		struct scalar_product_traits<long long,bertini::complex>
 		{
 	    	enum { Defined = 1 };
 	    	typedef bertini::complex ReturnType;
 		};
 
-		template<> 
-		struct scalar_product_traits<bertini::complex, long long> 
+		template<>
+		struct scalar_product_traits<bertini::complex, long long>
 		{
 	    	enum { Defined = 1 };
 	    	typedef bertini::complex ReturnType;
@@ -217,57 +223,57 @@ namespace Eigen {
 
 
 		//mpfr_float
-		template<> 
-		struct scalar_product_traits<bertini::mpfr_float,bertini::complex> 
+		template<>
+		struct scalar_product_traits<bertini::mpfr_float,bertini::complex>
 		{
 	    	enum { Defined = 1 };
 	    	typedef bertini::complex ReturnType;
 		};
 
-		template<> 
-		struct scalar_product_traits<bertini::complex, bertini::mpfr_float> 
+		template<>
+		struct scalar_product_traits<bertini::complex, bertini::mpfr_float>
 		{
 	    	enum { Defined = 1 };
 	    	typedef bertini::complex ReturnType;
 		};
 
 		//mpz_int
-		template<> 
-		struct scalar_product_traits<bertini::mpz_int,bertini::complex> 
+		template<>
+		struct scalar_product_traits<bertini::mpz_int,bertini::complex>
 		{
 	    	enum { Defined = 1 };
 	    	typedef bertini::complex ReturnType;
 		};
 
-		template<> 
-		struct scalar_product_traits<bertini::complex, bertini::mpz_int> 
+		template<>
+		struct scalar_product_traits<bertini::complex, bertini::mpz_int>
 		{
 	    	enum { Defined = 1 };
 	    	typedef bertini::complex ReturnType;
 		};
-		// template<> 
-		// struct scalar_product_traits<long,bertini::complex> 
+		// template<>
+		// struct scalar_product_traits<long,bertini::complex>
 		// {
 	 //    	enum { Defined = 1 };
 	 //    	typedef bertini::complex ReturnType;
 		// };
 
-		// template<> 
-		// struct scalar_product_traits<bertini::complex, long> 
+		// template<>
+		// struct scalar_product_traits<bertini::complex, long>
 		// {
 	 //    	enum { Defined = 1 };
 	 //    	typedef bertini::complex ReturnType;
 		// };
 
-		// template<> 
-		// struct scalar_product_traits<bertini::mpz_int,bertini::complex> 
+		// template<>
+		// struct scalar_product_traits<bertini::mpz_int,bertini::complex>
 		// {
 	 //    	enum { Defined = 1 };
 	 //    	typedef bertini::complex ReturnType;
 		// };
 
-		// template<> 
-		// struct scalar_product_traits<bertini::complex, bertini::mpz_int> 
+		// template<>
+		// struct scalar_product_traits<bertini::complex, bertini::mpz_int>
 		// {
 	 //    	enum { Defined = 1 };
 	 //    	typedef bertini::complex ReturnType;
@@ -277,13 +283,13 @@ namespace Eigen {
 
 		// template<typename Derived>
 		// CwiseUnaryOp<internal::scalar_multiple2_op<int,bertini::complex>, const Derived>
-		// operator*(int& lhs, const DenseBase<Derived>& rhs) 
+		// operator*(int& lhs, const DenseBase<Derived>& rhs)
 		// {
 		// 	return CwiseUnaryOp<internal::scalar_multiple2_op<int,bertini::complex>, const Derived>
 		// 		(internal::scalar_multiple2_op<bertini::complex,int>(lhs), rhs.derived());
 		// }
 
-		
+
 
 
 	} // re: namespace internal
@@ -301,7 +307,7 @@ namespace bertini {
 	template<typename NumType> using Vec = Eigen::Matrix<NumType, Eigen::Dynamic, 1>;
 	template<typename NumType> using Mat = Eigen::Matrix<NumType, Eigen::Dynamic, Eigen::Dynamic>;
 
-	
+
 	/**
 	\brief Checks whether the number of rows and columns are either 0
 	*/
@@ -314,7 +320,7 @@ namespace bertini {
 
 
 	/**
-	\brief Get the precision of an Eigen object. If the object is empty, it's the precision of a default-constructed Scalar.  If it actually has content, then it's the precision of the first element.  
+	\brief Get the precision of an Eigen object. If the object is empty, it's the precision of a default-constructed Scalar.  If it actually has content, then it's the precision of the first element.
 
 	If you require that the object be of uniform precision when you check, use PrecisionRequireUniform
 	*/
@@ -324,7 +330,7 @@ namespace bertini {
 	{
 		if (IsEmpty(v))
 			throw std::runtime_error("getting precision of empty object");
-		
+
 		return Precision(v(0,0));
 	}
 
@@ -360,21 +366,21 @@ namespace bertini {
 	void Precision(Eigen::MatrixBase<Derived> & v, unsigned prec)
 	{
 		using bertini::Precision;
-		
+
 		for (int ii=0; ii<v.cols(); ++ii)
 			for (int jj=0; jj<v.rows(); ++jj)
 				Precision(v(jj,ii),prec);
 	}
 
-	
+
 
 	/**
 	 Test a numbers being very small.
 
-	 Compares the number against machine epsilon (or software epsilon if a multiple precision type), times 100.  
+	 Compares the number against machine epsilon (or software epsilon if a multiple precision type), times 100.
 
 	 \note Machine epsilon for doubles is about 1e-16, for mpfr_float, it's 10^-current precision.
-	 
+
 	 \param testme The number to test
 
 	 \return true, if the number is very small.  False otherwise.
@@ -385,11 +391,11 @@ namespace bertini {
 	{
 		using std::abs;
 		return abs(testme) <= Eigen::NumTraits<T>::epsilon()*100;
-	} 
+	}
 
 	/**
 	\brief Check whether two values are very close to each other.
-	
+
 	\e The tolerance for being close
 	See \url http://www.boost.org/doc/libs/1_34_0/libs/test/doc/components/test_tools/floating_point_comparison.html, for example.
 	*/
@@ -407,9 +413,9 @@ namespace bertini {
 
 	/**
 	 Test two numbers for having large ratio.
-	
+
 	 The basis for comparison is Eigen's fuzzy precision, Eigen::NumTraits<T>::dummy_precision();
-	 
+
 	 \note For doubles, the threshold is 1e-12, for mpfr_float is 1e3*current precision.
 
 	 \param numerator The numerator for the ratio.
@@ -424,7 +430,7 @@ namespace bertini {
 		static_assert(!Eigen::NumTraits<T>::IsInteger, "IsLargeChange cannot be used safely on non-integral types");
 		using std::abs;
 		return abs(numerator/denomenator) >= 1/Eigen::NumTraits<T>::dummy_precision();
-	} 
+	}
 
 	enum class MatrixSuccessCode
 	{
@@ -434,7 +440,7 @@ namespace bertini {
 	};
 
 	/**
-	\brief Check the diagonal elements of an LU decomposition for small values and large ratios.  
+	\brief Check the diagonal elements of an LU decomposition for small values and large ratios.
 
 	\return Success if things are ok.  LargeChange or SmallValue if one is found.
 
@@ -453,7 +459,7 @@ namespace bertini {
 			// this loop won't test entry (0,0).  it's tested separately after.
 		for (unsigned int ii = LU.rows()-1; ii > 0; ii--)
 		{
-			if (IsSmallValue(LU(ii,ii))) 
+			if (IsSmallValue(LU(ii,ii)))
 			{
 				return MatrixSuccessCode::SmallValue;
 			}
@@ -465,7 +471,7 @@ namespace bertini {
 		}
 
 		// this line is the reason for the above assert on non-empty matrix.
-		if (IsSmallValue(LU(0,0))) 
+		if (IsSmallValue(LU(0,0)))
 		{
 			return MatrixSuccessCode::SmallValue;
 		}
@@ -482,11 +488,11 @@ namespace bertini {
 		using std::sqrt;
 		NumberType s, scale(1.0);
 		s = sqrt( (NumberType(1.0)-c) * (NumberType(1.0)+c) );
-		
-		
+
+
 		Eigen::Matrix<NumberType, Eigen::Dynamic, Eigen::Dynamic> A(mat_size,mat_size);
-		
-		
+
+
 		for (unsigned int ii=0; ii<mat_size; ii++) {
 			for (unsigned int jj=0; jj<ii; jj++) {
 				A(ii,jj) = NumberType(0.0);
@@ -495,19 +501,19 @@ namespace bertini {
 				A(ii,jj) = -c * NumberType(1.0);
 			}
 		}
-		
-		
+
+
 		for (unsigned int ii=0; ii<mat_size; ii++) {
 			A(ii,ii) += NumberType(1)+c;
 		}
-		
+
 		for (unsigned int jj=0; jj<mat_size; jj++){
 			for (unsigned int kk=0;kk<mat_size;kk++){
 				A(jj,kk) *= scale;
 			}
 			scale *= s;
 		}
-		
+
 		for (unsigned int jj=0;jj<mat_size;jj++){
 			for (unsigned int kk=0;kk<mat_size;kk++){
 				A(kk,jj)/= NumberType(jj) + NumberType(1);
@@ -557,21 +563,21 @@ namespace bertini {
 //
 // question asked by user Gabriel and answered by Shmuel Levine
 // answer code repeated here verbatim.
-// please update this comment if this code is changed, 
+// please update this comment if this code is changed,
 // and post the modifications to the above referenced post on SO.
 namespace boost{
     namespace serialization{
 
-        template<   class Archive, 
-                    class S, 
-                    int Rows_, 
-                    int Cols_, 
-                    int Ops_, 
-                    int MaxRows_, 
+        template<   class Archive,
+                    class S,
+                    int Rows_,
+                    int Cols_,
+                    int Ops_,
+                    int MaxRows_,
                     int MaxCols_>
         inline void save(
-            Archive & ar, 
-            const Eigen::Matrix<S, Rows_, Cols_, Ops_, MaxRows_, MaxCols_> & g, 
+            Archive & ar,
+            const Eigen::Matrix<S, Rows_, Cols_, Ops_, MaxRows_, MaxCols_> & g,
             const unsigned int version)
             {
                 int rows = g.rows();
@@ -582,16 +588,16 @@ namespace boost{
                 ar & boost::serialization::make_array(g.data(), rows * cols);
             }
 
-        template<   class Archive, 
-                    class S, 
+        template<   class Archive,
+                    class S,
                     int Rows_,
                     int Cols_,
-                    int Ops_, 
-                    int MaxRows_, 
+                    int Ops_,
+                    int MaxRows_,
                     int MaxCols_>
         inline void load(
-            Archive & ar, 
-            Eigen::Matrix<S, Rows_, Cols_, Ops_, MaxRows_, MaxCols_> & g, 
+            Archive & ar,
+            Eigen::Matrix<S, Rows_, Cols_, Ops_, MaxRows_, MaxCols_> & g,
             const unsigned int version)
         {
             int rows, cols;
@@ -601,16 +607,16 @@ namespace boost{
             ar & boost::serialization::make_array(g.data(), rows * cols);
         }
 
-        template<   class Archive, 
-                    class S, 
-                    int Rows_, 
-                    int Cols_, 
-                    int Ops_, 
-                    int MaxRows_, 
+        template<   class Archive,
+                    class S,
+                    int Rows_,
+                    int Cols_,
+                    int Ops_,
+                    int MaxRows_,
                     int MaxCols_>
         inline void serialize(
-            Archive & ar, 
-            Eigen::Matrix<S, Rows_, Cols_, Ops_, MaxRows_, MaxCols_> & g, 
+            Archive & ar,
+            Eigen::Matrix<S, Rows_, Cols_, Ops_, MaxRows_, MaxCols_> & g,
             const unsigned int version)
         {
             split_free(ar, g, version);
@@ -624,4 +630,3 @@ namespace boost{
 
 
 #endif
-
