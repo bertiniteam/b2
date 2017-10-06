8d2c872b0c5dfe10eec6ba6dadc501a43e6debe7
dani brake <danielthebrake@gmail.com>
2017-10-05 22:32:54 -0500

    now with a theme, grizzly

--- python/doc/source/conf.py
+++ python/doc/source/conf.py
@@ -160,3 +160,19 @@ texinfo_documents = [
 
 
 
+
+# theme stuff added 20171005 from https://github.com/guzzle/guzzle_sphinx_theme
+
+import guzzle_sphinx_theme
+
+html_theme_path = guzzle_sphinx_theme.html_theme_path()
+html_theme = 'guzzle_sphinx_theme'
+
+# Register the theme as an extension to generate a sitemap.xml
+extensions.append("guzzle_sphinx_theme")
+
+# Guzzle theme options (see theme.conf for more information)
+html_theme_options = {
+    # Set the name of the project to appear in the sidebar
+    "project_nav_name": "Project Name",
+}
