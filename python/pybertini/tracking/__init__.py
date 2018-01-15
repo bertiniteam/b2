f26225e11f670c203353dd348eb141655bc02017
danielle brake <brakeda@uwec.edu>
2018-01-15 15:05:58 -0600

    removed comment blob

--- python/pybertini/tracking/__init__.py
+++ python/pybertini/tracking/__init__.py
@@ -1,14 +1,5 @@
 """
 Tracking-specific things -- trackers, configs
-
-PyBertini -- Python bindings for Bertini2.
-
-This code is licensed under the GNU Public License, Version 3, with
-additional clauses under section 7 as permitted, to protect the 
-Bertini name.  See b2/licenses/ for a complete copy of the license,
-and the licenses of software upon which Bertini depends.
-
-See the source at https://github.com/bertiniteam/b2
 """
 
 from _pybertini.tracking import *
