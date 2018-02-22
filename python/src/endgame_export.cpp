bfe33ddd900ca47d63f68eae6c53eca3a9ade4dd
danielle brake <brakeda@uwec.edu>
2018-02-21 23:38:17 -0600

    named the arguments for endgame `run` functions

--- python/src/endgame_export.cpp
+++ python/src/endgame_export.cpp
@@ -55,8 +55,12 @@ namespace bertini{
 			.def("get_system",  &EndgameT::GetSystem,  return_internal_reference<>(),"Get the tracked system.  This is a reference to the internal system.")
 
 			.def("final_approximation", &EndgameT::template FinalApproximation<BCT>, return_internal_reference<>(),"Get the current approximation of the root, in the ambient numeric type for the tracker being used")
-			.def("run", RunDefaultTime<BCT>(),"Run the endgame, from start point and start time, to t=0.  Expects complex numeric type matching that of the tracker being used.")
-			.def("run", RunCustomTime<BCT>(),"Run the endgame, from start point and start time, to your choice of target time t.  Expects complex numeric type matching that of the tracker being used.")
+			.def("run", RunDefaultTime<BCT>(),
+				 (boost::python::arg("start_time"), "start_point"), 
+				 "Run the endgame, from start point and start time, to t=0.  Expects complex numeric type matching that of the tracker being used.")
+			.def("run", RunCustomTime<BCT>(),
+				 (boost::python::arg("start_time"), "start_point", "target_time"),
+				 "Run the endgame, from start point and start time, to your choice of target time t.  Expects complex numeric type matching that of the tracker being used.")
 			;
 		}
 
