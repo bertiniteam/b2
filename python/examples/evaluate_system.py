9e112602bc4f710272d6bf684e77a72fcbfcb064
silviana amethyst <1388063+ofloveandhate@users.noreply.github.com>
2023-07-12 13:48:46 +0200

    focus back to evaluating, not solving

--- python/examples/evaluate_system.py
+++ python/examples/evaluate_system.py
@@ -3,34 +3,22 @@ import numpy as np
 
 x = pb.Variable('x')
 y = pb.Variable('y')
+z = pb.Variable('z')
 
-vg = pb.VariableGroup([x,y])
+vg = pb.VariableGroup([x,y,z])
 
 sys = pb.System()
 
 sys.add_function(x-y)
 sys.add_function(x**2 + y**2 - 1)
+sys.add_function(5*x**3 + 16*x*y**4 - 17x*y*z - z**3)
 
 sys.add_variable_group(vg)
 
-C = pb.multiprec.Complex
+C = pb.multiprec.Complex # unpack a type for convenience
 
-variable_values = np.array([C(0), C(0)])
+variable_values = np.array([C(0), C(0), C(0)])
 
 result = sys.eval(variable_values)
 
-sys.homogenize()
-sys.auto_patch()
 
-variable_values = np.array([C(1), C(1), C(1)])
-
-sys.rescale_point_to_fit_patch_in_place(variable_values) # failing due to non-const reference
-
-
-solver = pb.nag_algorithm.ZeroDimCauchyAdaptivePrecisionTotalDegree(sys)
-
-solver.solve()
-
-for soln in solver.solutions():
-
-	print(sys.dehomogenize_point(soln))
